846fbaa07fc32fc84c7913f299ff449dddceafc3
nowsed30 <nowsedjahan30@gmail.com>
2018-08-29 00:28:48 +0600

    second(may be final touch)

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class product_detail(admin.ModelAdmin):
+    list_display=['title','pub_date','body','url']
+
+admin.site.register(Product)
